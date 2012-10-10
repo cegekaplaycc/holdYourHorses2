a93fd835f018fe45441bd0f09c92a579836b86e7
Ben Verbeken <ben@ostia.be>
2012-10-10 12:50:58 +0200

    added mockups

--- app/controllers/Mockups.java
+++ app/controllers/Mockups.java
@@ -4,10 +4,35 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.mockups.*;
 
+@SuppressWarnings("UnusedDeclaration")
 public class Mockups extends Controller {
 
     public static Result dashboard() {
         return ok(dashboard.render());
+    }
+
+    public static Result buySupplies() {
+        return ok(buySupplies.render());
+    }
+
+    public static Result futureRaceDetail() {
+        return ok(futureRaceDetail.render());
+    }
+
+    public static Result futureRaceOverview() {
+        return ok(futureRaceOverview.render());
+    }
+
+    public static Result horseDetail() {
+        return ok(horseDetail.render());
+    }
+
+    public static Result horseMarket() {
+        return ok(horseMarket.render());
+    }
+
+    public static Result pastRaceDetail() {
+        return ok(pastRaceDetail.render());
     }
 
 
