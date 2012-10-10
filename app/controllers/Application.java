2f0a4c537823eccb98f35b7b3816adc48b105fa3
Ben Verbeken <ben@ostia.be>
2012-10-10 12:25:23 +0200

    removed unused msg on index page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,7 +7,8 @@ import views.html.index;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
+
     }
 
 }
