d98d9fd4ab1c1deb6a24019a2034ee2e4b311f2e
jefklak <jefklak@gmail.com>
2012-10-11 10:01:13 +0200

    user aanmaken in model package

---
+++ app/models/User.java
@@ -0,0 +1,20 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jefklak
+ * Date: 11/10/12
+ * Time: 09:51
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class User extends Model {
+
+     public String name;
+
+
+}
---
+++ test/models/UserIntegrationTest.java
@@ -0,0 +1,14 @@
+package models;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jefklak
+ * Date: 11/10/12
+ * Time: 09:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserIntegrationTest {
+
+    }
