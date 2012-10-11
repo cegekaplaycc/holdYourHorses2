7e65d99fc9ad7cfaf636ceccd26b3720bac188c2
Matti Roloux <matti.roloux@gmail.com>
2012-10-11 11:10:48 +0200

    login form

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.Player;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
@@ -7,8 +9,11 @@ import views.html.index;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render());
+        return ok(index.render(new Form(Player.class)));
+    }
 
+    public static Result login() {
+        return null;
     }
 
 }
