093b490dc231391bcffd9eacb44a27698e58ea5d
bktid <tim.demeyer@cegeka.be>
2012-10-11 16:58:08 +0200

    tim: TODO's

--- app/services/HorseBreeder.java
+++ app/services/HorseBreeder.java
@@ -2,14 +2,11 @@ package services;
 
 import models.Horse;
 
-import com.avaje.ebean.Ebean;
-
-
 public class HorseBreeder {
-	
+
 	static final int MAX_AVAILABLE_HORSES = 20;
 	static final int INITIAL_PRICE = 1000;
-	
+
 	public void breedAvailableHorses() {
 		final int numberOfHorsesToBreed = determineNumberOfHorsesToBreed();
 		for (int i = 0; i < numberOfHorsesToBreed; i++) {
@@ -18,6 +15,6 @@ public class HorseBreeder {
 	}
 
 	private int determineNumberOfHorsesToBreed() {
-		return MAX_AVAILABLE_HORSES - Ebean.find(Horse.class).findRowCount();
+		return MAX_AVAILABLE_HORSES - Horse.findAvailableHorses().size();
 	}
 }
