def43624b6974867e67b2a4d93dc7f28a1f38c6c
jefklak <jefklak@gmail.com>
2012-10-11 10:29:54 +0200

    renamen naar player

--- app/models/Player.java
+++ app/models/Player.java
@@ -7,13 +7,13 @@ import javax.persistence.Id;
 
 /**
  * Created with IntelliJ IDEA.
- * User: jefklak
+ * Player: jefklak
  * Date: 11/10/12
  * Time: 09:51
  * To change this template use File | Settings | File Templates.
  */
 @Entity
-public class User extends Model {
+public class Player extends Model {
 
     @Id
     public Long id;
