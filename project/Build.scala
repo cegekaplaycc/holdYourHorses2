60dec3b5f964a401f184bcaec213d61351d06f4d
Kristof Lemmens <kristof.lemmens@cegeka.be>
2012-10-11 12:58:00 +0200

    benv: kristofl: RaceScheduler

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "commons-io" % "commons-io" % "2.4"
+      "commons-io" % "commons-io" % "2.4",      
+      "com.typesafe.akka" % "akka-testkit" % "2.0.2" % "test"      
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
