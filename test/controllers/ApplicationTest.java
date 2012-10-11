d409d943df9a2d77ad9142672171221de0727c99
Matti Roloux <matti.roloux@gmail.com>
2012-10-11 14:17:57 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/Global.java

--- test/controllers/ApplicationTest.java
+++ test/controllers/ApplicationTest.java
@@ -1,19 +1,51 @@
 package controllers;
 
+import com.google.common.collect.Maps;
+import models.Player;
+import org.apache.http.HttpStatus;
+import org.fest.assertions.Assertions;
 import org.junit.Test;
+import play.Routes;
+import play.data.Form;
+import play.mvc.Result;
 import play.test.Helpers;
+import scala.App;
 
-/**
- * Created with IntelliJ IDEA.
- * User: jefklak
- * Date: 11/10/12
- * Time: 12:08
- * To change this template use File | Settings | File Templates.
- */
+import javax.xml.ws.Response;
+import java.util.Map;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static play.mvc.Http.Status.UNAUTHORIZED;
+import static play.test.Helpers.*;
+import static play.test.Helpers.inMemoryDatabase;
+
 public class ApplicationTest {
 
     @Test
     public void loginRedirectsToLoginIfUserNotFound() {
+        running(fakeApplication(inMemoryDatabase()), new Runnable() {
+            public void run() {
+                Map<String, String> submittedData = Maps.newHashMap();
+                submittedData.put("username","kaatb");
+                submittedData.put("password","kaatc");
+                Result result = callAction(controllers.routes.ref.Application.login(), Helpers.fakeRequest().withFormUrlEncodedBody(submittedData));
+                assertThat(status(result)).isEqualTo(UNAUTHORIZED);
+            }
+        });
+    }
 
+    @Test
+    public void loginRedirectsToDashboardIfUserExists() {
+        running(fakeApplication(inMemoryDatabase()), new Runnable() {
+            public void run() {
+                Map<String, String> submittedData = Maps.newHashMap();
+                submittedData.put("username","matti");
+                submittedData.put("password","smatti");
+                Result result = routeAndCall(fakeRequest(POST, "/login").withFormUrlEncodedBody(submittedData));
+                //Result result = callAction(controllers.routes.ref.Application.login(), Helpers.fakeRequest().withFormUrlEncodedBody(submittedData));
+                assertThat(status(result)).isEqualTo(OK);
+                assertThat(Helpers.contentAsString(result)).contains("jeuj");
+            }
+        });
     }
 }
