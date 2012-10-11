3d9ed2736be15169c0fc96502ed7f3c544cbc0da
jefklak <jefklak@gmail.com>
2012-10-11 13:12:01 +0200

    begin test

---
+++ test/controllers/ApplicationTest.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jefklak
+ * Date: 11/10/12
+ * Time: 12:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class ApplicationTest {
+}
